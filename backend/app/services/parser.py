90596682ba5993d0f79d6095c15a8260a957ef1b
Brian Byun <brianubyun@gmail.com>
2026-01-24 18:26:35 -0800

    changed comments for better explanation

--- backend/app/services/parser.py
+++ backend/app/services/parser.py
@@ -2,30 +2,16 @@ import icalendar
 
 #User uploads their .ics file and I believe we would need some function to get the .ics file upload
 
-""" One possible format of an .ics file:
-BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//hacksw/handcal//NONSGML v1.0//EN
-BEGIN:VEVENT
-UID:uid1@example.com
-ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com
-DTSTAMP:19970701T100000Z
-DTSTART:19970714T170000Z
-DTEND:19970715T040000Z
-SUMMARY:Bastille Day Party
-GEO:48.85299;2.36885
-END:VEVENT
-END:VCALENDAR
-Link: https://en.wikipedia.org/wiki/ICalendar - includes other components that can be included in an .ics file 
+""" Look in winter2026.ics for an example of the format of .ics file
 
 Link: https://icalendar.org/iCalendar-RFC-5545/3-3-10-recurrence-rule.html - formatting on RRULE or recurrence rules
 
-Multiple VEVENTS in one file (for every event, there is a VEVENT) """
+Multiple VEVENTS in one file (for every event/class, there is a VEVENT) """
 
 def parse_ics(file_name: str) -> list[dict]: #Gets file and parses through it to extract relevant components to a dictionary and returns a list of a schedule's classes and components
     events = [] 
 
-    with open(file_name, 'rb') as file: #Opens and reads .ics file to parse and extract relevant components
+    with open(file_name, 'rb') as file: #Opens and reads .ics file to parse and extract relevant components; 'rb' or read binary for icalendar format
         cal = icalendar.Calendar.from_ical(file.read())
 
         for components in cal.walk():
@@ -33,13 +19,13 @@ def parse_ics(file_name: str) -> list[dict]: #Gets file and parses through it to
                 event = {}
                 event["class"] = components.get("SUMMARY")
                 event["classroom"] = components.get("LOCATION")
-                event["start_time"] = components.get("DTSTART").dt
+                event["start_time"] = components.get("DTSTART").dt #dt allows for better readability of datetime
                 event["end_time"] = components.get("DTEND").dt
 
-                if components.get("RRULE"): #Specific case to get the days from recurrence
-                    if "BYDAY" in components.get("RRULE"): #BYDAY means which days of the week the event/class happens 
+                if components.get("RRULE"): #Get which days of the week the class happens and when quarter ends
+                    if "BYDAY" in components.get("RRULE"): #Get days in component of RRULE with BYDAY key 
                         event["days"] = components.get("RRULE")["BYDAY"]
-                    if "UNTIL" in components.get("RRULE"): #Get the end date of the recurrence
+                    if "UNTIL" in components.get("RRULE"): #Get the end date of quarter with UNTIL key in RRULE
                         event["until"] = components.get("RRULE")["UNTIL"][0]
 
                 events.append(event) 
