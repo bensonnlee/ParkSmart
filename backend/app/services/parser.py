1b3b186ff44336833482fd5e0a46cb1090f70fda
Brian Byun <brianubyun@gmail.com>
2026-01-24 23:59:29 -0800

    ran ruff formating

--- backend/app/services/parser.py
+++ backend/app/services/parser.py
@@ -1,6 +1,6 @@
 import icalendar
 
-#User uploads their .ics file and I believe we would need some function to get the .ics file upload
+# User uploads their .ics file and I believe we would need some function to get the .ics file upload
 
 """ Look in winter2026.ics for an example of the format of .ics file
 
@@ -10,32 +10,52 @@ Link: https://stackoverflow.com/questions/3408097/parsing-files-ics-icalendar-us
 
 Multiple VEVENTS in one file (for every event/class, there is a VEVENT) """
 
-def parse_ics(file_name: str) -> list[dict]: #Gets file and parses through it to extract relevant components to a dictionary and returns a list of a schedule's classes and components
-    events = [] 
 
-    with open(file_name, 'rb') as file: #Opens and reads .ics file to parse and extract relevant components; 'rb' or read binary for icalendar format
+def parse_ics(
+    file_name: str,
+) -> list[
+    dict
+]:  # Gets file and parses through it to extract relevant components to a dictionary and returns a list of a schedule's classes and components
+    events = []
+
+    with (
+        open(file_name, "rb") as file
+    ):  # Opens and reads .ics file to parse and extract relevant components; 'rb' or read binary for icalendar format
         cal = icalendar.Calendar.from_ical(file.read())
 
         for components in cal.walk():
-            if components.name == "VEVENT": #Found a single event/class so set up a dictionary of relevant components and add to events list
+            if (
+                components.name == "VEVENT"
+            ):  # Found a single event/class so set up a dictionary of relevant components and add to events list
                 event = {}
                 event["class"] = components.get("SUMMARY")
                 event["classroom"] = components.get("LOCATION")
-                event["start_time"] = components.get("DTSTART").dt #dt allows for better readability of datetime
+                event["start_time"] = components.get(
+                    "DTSTART"
+                ).dt  # dt allows for better readability of datetime
                 event["end_time"] = components.get("DTEND").dt
 
-                if components.get("RRULE"): #Get which days of the week the class happens and when quarter ends
-                    if "BYDAY" in components.get("RRULE"): #Get days in component of RRULE with BYDAY key 
+                if components.get(
+                    "RRULE"
+                ):  # Get which days of the week the class happens and when quarter ends
+                    if "BYDAY" in components.get(
+                        "RRULE"
+                    ):  # Get days in component of RRULE with BYDAY key
                         event["days"] = components.get("RRULE")["BYDAY"]
-                    if "UNTIL" in components.get("RRULE"): #Get the end date of quarter with UNTIL key in RRULE
+                    if "UNTIL" in components.get(
+                        "RRULE"
+                    ):  # Get the end date of quarter with UNTIL key in RRULE
                         event["until"] = components.get("RRULE")["UNTIL"][0]
 
-                events.append(event) 
+                events.append(event)
     return events
 
-#Test with a sample .ics file from winter 2026 schedule 
+
+# Test with a sample .ics file from winter 2026 schedule
 if __name__ == "__main__":
-    events = parse_ics("winter2026.ics")
+    events = parse_ics(
+        "C:\\Users\\brian\\OneDrive\\Documents\\UCR\\UCR Fourth Year\\WINTER QUARTER !\\CS 179M\\ParkSmart\\backend\\app\\services\\winter2026.ics"
+    )
 
     for i, event in enumerate(events):
         print("Classroom: " + str(event["classroom"]))
@@ -45,4 +65,3 @@ if __name__ == "__main__":
         print("Days: " + str(event.get("days")))
         print("Until: " + str(event.get("until")))
         print("-----------------------")
-        
