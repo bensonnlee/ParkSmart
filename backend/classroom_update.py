79599b28d662888dbfff03e427a9a7d96e99b795
Aditya Verma <adityaverma@Adityas-MacBook-Pro.local>
2026-02-06 20:35:37 -0800

    Updated locations file

--- backend/classroom_update.py
+++ backend/classroom_update.py
@@ -5,8 +5,13 @@ df = df.loc[:, kept_cols]
 
 is_parking_lot = df["Type"].str.contains(r"parking\s*lot|^lot\b", case=False, na=False)
 is_building = df["Type"].str.contains(r"Building", case=False, na=False) 
+is_dining = df["Type"].str.contains("Dining", case=False, na=False)
+
 df["Structure"] = df["Type"]
 df.loc[is_parking_lot, "Structure"] = "Parking Lot"
 df.loc[is_building, "Structure"] = "Classroom"
+df.loc[is_dining, "Structure"] = "Dining/Retail"
 
 print(df.head())
+
+df.to_csv("Locations_updated.csv", index=False)
